caa8e827faa0bf2d1bff30337c93b437906a9bd3
priyanka-bhoir <er.priyankabhoir1@gmail.com>
2022-05-21 11:56:25 +0530

    final with clickable

--- app/src/main/java/com/priyanka/composeexample/MainActivity.kt
+++ app/src/main/java/com/priyanka/composeexample/MainActivity.kt
@@ -3,13 +3,20 @@ package com.priyanka.composeexample
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.RectangleShape
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.priyanka.composeexample.ui.theme.ComposeExampleTheme
 
 class MainActivity : ComponentActivity() {
@@ -23,15 +30,30 @@ class MainActivity : ComponentActivity() {
                     color = MaterialTheme.colors.background
                 ) {
                     Greeting("Android")
+
+                }
                 }
             }
-        }
+
+
     }
 }
 
 @Composable
 fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+//    Text(text = "Hello $name!")
+    Column(
+        Modifier.background(Color.Magenta)
+            .border(12.dp, Color.Blue, RectangleShape)
+            .fillMaxHeight(16f)
+            .padding(5.dp)
+            .fillMaxWidth()
+    ){
+        Text(text = "The Great Priyanka",
+            Modifier.clickable {     })
+
+
+    }
 }
 
 @Preview(showBackground = true)
